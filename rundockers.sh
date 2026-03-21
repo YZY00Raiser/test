e2636f982f22f5246d827716a7531546f7271caf
ZY Y <99327288+YZY00Raiser@users.noreply.github.com>
2026-03-21 10:19:32 +0800

    add

---
+++ rundockers.sh
@@ -0,0 +1,32 @@
+docker run -itd --ipc=host \
+--device=/dev/davinci0 \
+--device=/dev/davinci1 \
+--device=/dev/davinci2 \
+--device=/dev/davinci3 \
+--device=/dev/davinci4 \
+--device=/dev/davinci5 \
+--device=/dev/davinci6 \
+--device=/dev/davinci7 \
+--device=/dev/davinci8 \
+--device=/dev/davinci9 \
+--device=/dev/davinci10 \
+--device=/dev/davinci11 \
+--device=/dev/davinci12 \
+--device=/dev/davinci13 \
+--device=/dev/davinci14 \
+--device=/dev/davinci15 \
+--device=/dev/davinci_manager \
+--device=/dev/devmm_svm \
+--device=/dev/hisi_hdc \
+-v /usr/local/Ascend/driver:/usr/local/Ascend/driver \
+-v /usr/local/Ascend/add-ons/:/usr/local/Ascend/add-ons/ \
+-v /usr/local/sbin/npu-smi:/usr/local/sbin/npu-smi \
+-v /usr/local/sbin/:/usr/local/sbin/ \
+-v /var/log/npu/conf/slog/slog.conf:/var/log/npu/conf/slog/slog.conf \
+-v /var/log/npu/slog/:/var/log/npu/slog \
+-v /var/log/npu/profiling/:/var/log/npu/profiling \
+-v /var/log/npu/dump/:/var/log/npu/dump \
+-v /var/log/npu/:/usr/slog \
+-v /var/queue_schedule:/var/queue_schedule \
+ascend-cann:8.1.RC1-ubuntu-pt \
+/bin/bash
